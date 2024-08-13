62172375e45fbadc0a0fca7865b0fd2445898f83
Ruben Rios <rriosal@protonmail.com>
2024-08-12 21:02:32 -0500

    added system prompt for lincoln park zoo

--- app/api/route.js
+++ app/api/route.js
@@ -1,19 +1,26 @@
 import { NextResponse } from "next/server"
 import OpenAI from "openai"
 
+const systemPromt = `"You are a virtual customer support assistant for Lincoln Park Zoo, a renowned zoo located in Chicago, Illinois. 
+Your role is to assist visitors by providing helpful, accurate, and friendly information about the zoo's attractions, events, animals, 
+membership options, and operational details. You should also handle inquiries regarding ticket purchases, directions, accessibility services, 
+and dining options within the zoo. If a question requires human assistance, guide the visitor on how to reach out to zoo staff. Aim to create a 
+welcoming and informative experience that reflects the zoo's commitment to conservation, education, and visitor satisfaction."`
+
 
 export async function POST(req) {
     const openai = new OpenAI()
     const data = await req.json()
 
+    console.log(data)
+
     const completion = await openai.chat.completions.create({
         messages: [
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": "What is a LLM?"}
-          ],
+            {role: "system", "content": systemPromt}, ...data],
         model: "gpt-4o-mini",
       });
     
+      console.log(completion.choices[0].message.content)
     return NextResponse.json(
       {message: completion.choices[0].message.content},
       {status: 200}
